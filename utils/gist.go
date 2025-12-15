0568db4fece119fb4ca240d63a8fc215070aae6c
RyannKim327 <rksesgundo123@gmail.com>
2025-12-15 20:55:43 +0800

    12-15-25 20:55 [Added Gist Control and other stuffs]

---
+++ utils/gist.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+var URL = fmt.Sprintf("https://api.github.com/gists/%s", os.Getenv("API_KEY"))
+
+func access(params AccessApi) GistResponseHandler {
+	if params.Method == "" {
+		params.Method = "GET"
+	}
+
+	req, err := http.NewRequest(params.Method, params.URL, params.Body)
+	if err != nil {
+		return GistResponseHandler{Error: err}
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("API_KEY")))
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-Github-Api-Version", "2022-11-28")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return GistResponseHandler{Error: err}
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return GistResponseHandler{Error: err}
+	}
+
+	return GistResponseHandler{Response: body}
+}
+
+func Get(url string) GistResponseHandler {
+	resp := access(AccessApi{
+		URL: URL,
+	})
+	return resp
+}
+
+func Post(url string, data map[string]string) GistResponseHandler {
+	jsonBody, _ := json.Marshal(data)
+
+	resp := access(AccessApi{
+		Method: "POST",
+		URL:    URL,
+		Body:   bytes.NewBuffer(jsonBody),
+	})
+	return resp
+}
--- utils/structures.go
+++ utils/structures.go
@@ -1,9 +1,24 @@
 package utils
 
-import "github.com/gin-gonic/gin"
+import (
+	"io"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Route struct {
 	Method  string
 	Path    string
 	Handler gin.HandlerFunc
+}
+
+type GistResponseHandler struct {
+	Error    error
+	Response []byte
+}
+
+type AccessApi struct {
+	Method string
+	URL    string
+	Body   io.Reader
 }
